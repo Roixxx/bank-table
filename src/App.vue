5f90d0cf3104d3ecd13a846cbf8a53dd68c9d8ac
Your Name <vosroix@gmail.com>
2022-01-03 13:34:12 +0500

    del

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,14 @@
 <template>
-	<div class="container column">
+  <div class="container">
+    <div class="card">
 
-
-		<AppFormControl @addItem="addItem" />
-
-		<AppCard :fields="fields" />
-
-		
-
-	</div>
-	<AppComments></AppComments>
-
+    </div>
+  </div>
 </template>
 
 <script>
-
-import AppFormControl from "./components/AppFormControl";
-import AppCard from "./components/AppCard";
-import AppComments from "./components/AppComments";
-
-
-
 export default {
 
-	data() {
-		return {
-
-			fields: [],
-		}
-	},
-
-
-	methods: {
-		addItem(field) {
-			this.fields.push(field);
-		}
-	},
-
-	components: {
-		AppFormControl,
-		AppCard,
-		AppComments,
-	},
-
 }
-
 </script>
 
-
-
-<style>
-.avatar {
-	display: flex;
-	justify-content: center;
-}
-
-.avatar img {
-	width: 150px;
-	height: auto;
-	border-radius: 50%;
-}
-</style>
