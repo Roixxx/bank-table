10a4e085cfdaab418509d397adf6244b8841688b
Your Name <vosroix@gmail.com>
2022-01-03 18:06:24 +0500

    del

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
 	<div class="container">
 		<div class="card">
-			<h2 v-color:[type].myBlink="myColor">Разговор про директивы</h2>
-			<div class="form-control">
-				<label for="inp">Активный по умолчанию</label>
-				<input type="text" id="inp" v-focus>
-			</div>
-
-			<button class="btn" @click="myColor = 'green'">Зеленый цвет</button>
-			<button class="btn" @click="type = type === 'color' ? 'backgroundColor' : 'color' ">Переключить тип</button>
-			{{type}}
 
 		</div>
 
@@ -24,37 +15,11 @@ export default {
 
 	data() {
 		return {
-			myColor: 'darkred',
-			type: 'color',
+
 		}
 	},
 
-	directives: {
-		focus: {
-			mounted(el) {
-				el.focus();
-			}
-		},
-		color: {
 
-			mounted(el, binding) {
-				el.style[binding.arg] = binding.value;
-				
-				let flag = true;
-				if (binding.modifiers.myBlink) {
-					setInterval( () => {
-
-						el.style.color = flag ? '#fff' : binding.instance.myColor;
-						flag = !flag;
-					}, 1000)
-				}
-			},
-
-			updated(el, binding) {
-				el.style[binding.arg] = binding.value;
-			}
-		}
-	},
 
 	components: {
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
 
-createApp(App)
-	// .directive('focus', {       глобальная регистрация директивы
-	// 	mounted(el) {
-	// 		el.focus();
-	// 	}
-	// })
-	.mount('#app')
+createApp(App).mount('#app')
