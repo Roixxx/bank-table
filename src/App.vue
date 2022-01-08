11ccabedcf168b5109b152b1dff08df03edad1c0
Max <vosroix@gmail.com>
2022-01-08 23:21:41 +0500

    del

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <the-navbar></the-navbar>
-  <div class="container with-nav">
-    <div class="card">
-      <h1>{{uppercaseTitle}}</h1>
-      <h2>Счетчик {{ counter }} ({{ doubleCounter }})</h2>
-      <button class="btn primary" @click="add">Добавить</button>
-      <button class="btn danger" @click="incrementAsync({value: 10,delay: 200})">Добавить 10</button>
-    </div>
-  </div>
+	<div class="container">
+		<div class="card">
+			<h1>Vue Composition Api</h1>
+			<hr>
+			<p>Название: <strong>{{ name }}</strong></p>
+			<p>Версия: <strong>{{ version }}</strong></p>
+
+			<button class="btn" @click="changeInfo">Изменить</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapActions } from 'vuex'
-import TheNavbar from './TheNavbar'
+
+import {ref, reactive, toRefs} from 'vue';  // reactive только для объектов
+									// ref для всего остального
 
 export default {
-  components: {TheNavbar},
-  computed: {
-    ...mapGetters(['uppercaseTitle']),
-    ...mapGetters('count', ['counter', 'doubleCounter'])
-  },
-  methods: {
-    ...mapMutations({add: 'count/increment'}),
-    ...mapActions('count', ['incrementAsync']),
-  }
+	setup() {
+		const name = ref('VueJS');
+		const version = ref(3);
+
+		// const myObj = reactive({						< работа с объектом
+		// 	name: 'VueJs',
+		// 	version: 3,
+		// })
+
+		function changeInfo() {
+			name.value = 'Vue lalal';
+			version.value = 'Vue 4';
+
+			// myObj.name = 'vue js';					< работа с объектом
+			// myObj.version = 42;
+		}
+
+		return {
+
+			//...toRefs(myObj),							< работа с объектом
+
+			name,
+			version,
+			changeInfo
+		}
+	},
+
+	// data() {
+	// 	return {
+	// 		name: 'VueJS',
+	// 		version: 3
+	// 	}
+	// },
+	// methods: {
+	// 	changeInfo() {
+	// 		this.name = 'Vue JS!'
+	// 		this.version = 4
+	// 	}
+	// }
 }
 </script>
